45272449be67fe56f8f2625181a638423ae2612c
Aloshukur Usmonov <usmonovaloshukur@gmail.com>
2023-06-23 00:20:03 +0500

    25-dars
    Ushbu darsda son topish o'yini kodi joylandi

---
+++ son-topish-o'yini.py
@@ -0,0 +1,63 @@
+import random as r
+print("Keling o'ylagan sonni topish o'ynaymiz!")
+
+def son_top(n=10):
+    sonlar = list(range(1,n+1))
+    x=r.choice(sonlar)
+    y = int(input(f"1 dan {n} gacha son o'yladim. Topa olasizmi?\n>>>"))
+    urinish_soni = 0
+    while True:
+        urinish_soni+=1
+        if y>x:
+            y=int(input("Xato, men o'ylagan son bundan kichikroq. Yana harakat qiling:\n>>>"))
+        elif y<x:
+            y=int(input("Xato, men o'ylagan son bundan kattaroq. Yana harakat qiling:\n>>>"))
+        else:
+            y=f"Topdingiz {y} sonini o'ylagan edim. {urinish_soni} ta taxmin bilan topdingiz. Tabriklayman!"
+            break
+    print(y)
+    return urinish_soni
+
+def son_top_pc(n=10):
+    input(f"1 dan {n} gacha son o'ylang. Men topishga harakat qilaman. O'ylagan bo'lsangiz istalgan tugmani bosing ")
+    quyi = 1
+    yuqori = n
+    urinish_soni=0
+    while True:
+        urinish_soni+=1
+        if quyi!=yuqori:
+            taxmin = r.randint(quyi,yuqori)
+        else:
+            taxmin = quyi
+        javob = input(f"Siz {taxmin} sonini o'yladingiz: to'g'ri(t), katta(+), kichik(-)".lower())
+
+        if javob == '-':
+            yuqori = taxmin - 1
+        elif javob == '+':
+            quyi = taxmin + 1
+        else:
+            break
+    print(f"Men {urinish_soni} ta taxmin bilan topdim!")
+    return urinish_soni
+
+def play(n=10):
+    taxminlar = son_top(n)
+    taxminlar_pc = son_top_pc(n)
+    yana  = True
+    while yana:
+        if taxminlar>taxminlar_pc:
+            print("Yutqazdingiz! Men yutdim")
+        elif taxminlar<taxminlar_pc:
+            print("Siz yutdingiz!")
+        else:
+            print("Durrang!")
+        yana = int(input("Yana o'ynaysizmi ha(1), yo'q(0)"))
+play()
+
+
+
+
+
+
+
+
