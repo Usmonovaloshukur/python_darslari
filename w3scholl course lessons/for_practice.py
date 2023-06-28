91583a03f9d5f45a026769a7c58574e0a062f6f8
Aloshukur Usmonov <usmonovaloshukur@gmail.com>
2023-06-28 15:24:05 +0500

    Create for_practice.py

---
+++ w3scholl course lessons/for_practice.py
@@ -0,0 +1,55 @@
+
+# #################### unpack a list  ( listni ochish )
+#
+# mevalar = ["olma", "anor", "uzum"]
+#
+# x, y, z = mevalar
+# print(x)
+# print(y)
+# print(z)
+# # natija:
+# # olma
+# # anor
+# # uzum
+
+
+
+# ###############################  global variables
+
+# x = "awesome"
+#
+# def myfunk():
+#     print("Python is", x)
+# myfunk()
+#
+#
+# x = "awesome"
+#
+# def myfunc1():
+#     x = "fantastic"
+#     print("Python is " + x)
+#
+# myfunc1()
+#
+# print("Python is " + x)
+
+# def myfunc2():
+#   global x
+#   x = "fantastic"
+#
+# myfunc2()
+#
+# print("Python is " + x)
+
+# ##### change the value of variable by using function
+
+# x = "awesome"
+#
+# def myfunc():
+#   global x
+#   x = "fantastic"
+#
+# myfunc()
+#
+# print("Python is " + x)
+
